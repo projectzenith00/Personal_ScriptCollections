f0eb4f29e4bf6f3923168ddfe8a26ae2342851ef
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2024-04-14 18:42:09 +0800

    Create exportActiveADUsers.ps1

---
+++ PowerShell_scripts/exportActiveADUsers.ps1
@@ -0,0 +1,5 @@
+#Export to CSV all Active Users from AD using Powershell
+#Extracts the User's NT Username, Full Name and Organization Unit.
+#Saves the exported list on the current login user's Desktop Folder
+
+Get-ADUser -filter 'enabled -eq $true' -SearchBase "OU=Accounts,OU=Employees,DC=sophi-outsourcing,DC=local" -properties CanonicalName,Enabled | select SamAccountName,Name,@{n='OU';e={($_.CanonicalName  -split "/")[-2]}} | Export-CSV $env:USERPROFILE\Desktop\ADusers.csv -notypeinformation
