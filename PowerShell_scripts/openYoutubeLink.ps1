73b05d84c611608a5a32415c38eb9280659ecaed
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2024-05-22 11:16:44 +0800

    Update openYoutubeLink.ps1

--- PowerShell_scripts/openYoutubeLink.ps1
+++ PowerShell_scripts/openYoutubeLink.ps1
@@ -1,3 +1,4 @@
 Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList '--kiosk', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 Start-Sleep -Seconds 7;
 [System.Windows.Forms.SendKeys]::SendWait('f');
+#[System.Windows.Forms.SendKeys]::SendWait('{F11}')
