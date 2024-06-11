ef7980ddfef234a029e02799a06883687c4e975d
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2024-06-11 20:24:17 +0800

    Update configure_PlugandPlayDevices.ps1

--- PowerShell_scripts/configure_PlugandPlayDevices.ps1
+++ PowerShell_scripts/configure_PlugandPlayDevices.ps1
@@ -1,8 +1,10 @@
 # Enable, Disable or Check plug and play devices (e.g. USB Keyboard, USB Mouse, USB Headset)
 # To get all currently connected Plug-in-Play devices.
-Get-PnpDevice  -PresentOnly | Select-Object -Property InstanceID, FriendlyName, Class, Status | Sort-Object -Property Class
+Get-PnpDevice  -PresentOnly | Select-Object -Property InstanceID, FriendlyName, Class, Status | Sort-Object -Property Class;
 
 # To get all currently connected Speaker(Media) and Microphone(AudioEndpoint) Plug-in-Play devices.
-Get-PnpDevice -Class 'Media', 'AudioEndpoint' -PresentOnly -Status 'OK' | Select-Object -Property InstanceID, FriendlyName, Class | Sort-Object -Property Class
+Get-PnpDevice -Class 'Media', 'AudioEndpoint' -PresentOnly -Status 'OK' | Select-Object -Property InstanceID, FriendlyName, Class | Sort-Object -Property Class;
 
-Disable-PnpDevice -InstanceId 'replace-with-correct-InstanceId'
+#Example: Disable keyboard
+
+Get-PnpDevice  -PresentOnly | WHERE { $_.FriendlyName -Like "*KEYBOARD*" } | Select-Object -Property InstanceID;
