7079f117330a8ec7dec469eb7314d1eeccf649c2
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2024-06-14 04:19:46 +0800

    Create getFolderSize.ps1

---
+++ PowerShell_scripts/getFolderSize.ps1
@@ -0,0 +1,7 @@
+$targetDirectory = "C:\Users";
+
+Get-ChildItem -force $targetDirectory -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | ForEach-Object {
+    $len = 0;
+    Get-ChildItem -recurse -force $_.fullname -ErrorAction SilentlyContinue | ForEach-Object { $len += $_.length }
+    $_.fullname, '{0:N2} GB' -f ($len / 1Gb) 
+}
