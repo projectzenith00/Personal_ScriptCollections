2c558ef642334f54de4b2c6f96b2bf3ded6b0b7c
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2024-08-01 09:11:52 +0800

    Update autoDeleteNTUserFolder.ps1

--- PowerShell_scripts/autoDeleteNTUserFolder.ps1
+++ PowerShell_scripts/autoDeleteNTUserFolder.ps1
@@ -1,7 +1,9 @@
 # A PowerShell script that will automatically confirm the deletion of the NT User folder in C:\Users directory using the "-Confirm:$false" parameter. 
 # This script is helpful if you want to avoid any file corruption to the NT User's folder in case they need to login to the same device/computer in the future.
+# Make sure that you don't replace the admin and Public folder names in the array list. 
+# Only start replacing the fuser.
 
-$excludedFolders = @("aatest", "mmartin", "bbulayagon")
+$excludedFolders = @("admin", "Public", "fuser", "luser")
 $allFolders = Get-ChildItem -Path "C:\Users\" -Directory
 
 foreach ($folder in $allFolders) {
