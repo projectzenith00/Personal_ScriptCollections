90f4c39545366d54d067700b1c780bb223502c76
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2024-08-01 08:30:22 +0800

    Update exportActiveADUsers.ps1

--- PowerShell_scripts/exportActiveADUsers.ps1
+++ PowerShell_scripts/exportActiveADUsers.ps1
@@ -2,4 +2,8 @@
 #Extracts the User's NT Username, Full Name and Organization Unit.
 #Saves the exported list on the current login user's Desktop Folder
 
-Get-ADUser -filter 'enabled -eq $true' -SearchBase "OU=Accounts,OU=Employees,DC=sophi-outsourcing,DC=local" -properties CanonicalName,Enabled | select SamAccountName,Name,@{n='OU';e={($_.CanonicalName  -split "/")[-2]}} | Export-CSV $env:USERPROFILE\Desktop\ADusers.csv -notypeinformation 
+Get-ADUser -filter 'enabled -eq $true' 
+-SearchBase "OU=Accounts,OU=Employees,DC=sophi-outsourcing,DC=local" 
+-properties CanonicalName,Enabled | 
+select SamAccountName,Name,@{n='OU';e={($_.CanonicalName  -split "/")[-2]}} | 
+Export-CSV $env:USERPROFILE\Desktop\ADusers.csv -notypeinformation 
