45ba4052bb51fd22bd13172ca317536c2f63ee0d
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2024-10-09 15:23:20 -0700

    Update pingLogger.ps1
    Repaced C:\EnvisionMSP\logs with C:\Drive\Path\Folder\pingLogs.txt

--- PowerShell_scripts/pingLogger.ps1
+++ PowerShell_scripts/pingLogger.ps1
@@ -1,5 +1,5 @@
-# PowerShell Script that will log the ping results with Time and Date Stamp to Text file named "pinglog.txt" inside the "C:\EnvisionMSP\logs" folder.
+# PowerShell Script that will log the ping results with Time and Date Stamp to Text file named "pinglog.txt" inside the "C:\Drive\Path\Folder" folder.
 # If either or both the text file or path/folders doesn't exist, this script will create it.
 # If the the text file already exist, it will add the new log entries below the the last log entries.
 
-ping.exe -t 8.8.8.8 | ForEach {"{0} - {1}" -f (Get-Date),$_} | Out-File -FilePath "C:\Users\KentSalamoding\Desktop\pinglogs.txt" -Append;
+ping.exe -t 8.8.8.8 | ForEach {"{0} - {1}" -f (Get-Date),$_} | Out-File -FilePath "C:\Drive\Path\Folder\pinglogs.txt" -Append;
