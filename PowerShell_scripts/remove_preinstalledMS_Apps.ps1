6bc7cff680546206f34e6cd4d9fc2c9987352a68
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2025-02-10 12:44:54 -0800

    Update remove_preinstalledMS_Apps.ps1
    Added Package Name Search Function

--- PowerShell_scripts/remove_preinstalledMS_Apps.ps1
+++ PowerShell_scripts/remove_preinstalledMS_Apps.ps1
@@ -1,19 +1,36 @@
-winget list -q "MS";
+#winget list -q "MS";
 
-$packageIDs=@(
-'ARP\Machine\X64\O365HomePremRetail - en-us',
-'ARP\Machine\X64\O365HomePremRetail - es-es',
-'ARP\Machine\X64\O365HomePremRetail - fr-fr',
-'ARP\Machine\X64\O365HomePremRetail - pt-br',
-'ARP\Machine\X64\OneNoteFreeRetail - en-us',
-'ARP\Machine\X64\OneNoteFreeRetail - es-es',
-'ARP\Machine\X64\OneNoteFreeRetail - fr-fr',
-'ARP\Machine\X64\OneNoteFreeRetail - pt-br',
-'Microsoft Teams',
-'Microsoft.OneDrive'
-)
+function getByPackageIDs {
+    $packageIDs=@(
+    'ARP\Machine\X64\O365HomePremRetail - en-us',
+    'ARP\Machine\X64\O365HomePremRetail - es-es',
+    'ARP\Machine\X64\O365HomePremRetail - fr-fr',
+    'ARP\Machine\X64\O365HomePremRetail - pt-br',
+    'ARP\Machine\X64\OneNoteFreeRetail - en-us',
+    'ARP\Machine\X64\OneNoteFreeRetail - es-es',
+    'ARP\Machine\X64\OneNoteFreeRetail - fr-fr',
+    'ARP\Machine\X64\OneNoteFreeRetail - pt-br',
+    'Microsoft Teams',
+    'Microsoft.OneDrive'
+    )
 
-foreach ($packageID in $packageIDs) 
-    {
-        winget uninstall -e --id $packageID --silent --accept-source-agreements;
-    } 
+    foreach ($packageID in $packageIDs) 
+        {
+            winget uninstall -e --id $packageID --silent --accept-source-agreements;
+        } 
+}
+
+
+function getByPackageNames {
+    $packageNames=@(
+        'Microsoft Teams (personal)'
+        )
+    foreach ($packageName in $packageNames) 
+        {
+            winget uninstall -e --name $packageName --silent --accept-source-agreements --accept-package-agreements;
+        } 
+}
+
+
+getByPackageIDs;
+getByPackageNames;
