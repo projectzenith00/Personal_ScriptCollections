b72762501f0fc1e71455c0233c7eaaad31f6baf6
Kent Rogell <50953950+projectzenith00@users.noreply.github.com>
2025-05-08 15:41:40 -0400

    Create scoffPRNK.ps1

---
+++ PowerShell_scripts/PRNK/scoffPRNK.ps1
@@ -0,0 +1,3 @@
+(Add-Type '[DllImport("user32.dll")]public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2);
+Start-Sleep -Seconds 5;
+Exit
